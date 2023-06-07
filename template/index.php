43c75e71305f6d44ff9c62e880f658b4d1c5217e
parul <cseprs@gmail.com>
2023-06-07 10:24:48 +0530

    revision second

--- template/index.php
+++ template/index.php
@@ -27,12 +27,12 @@ echo $OUTPUT->header();
 
 
 
-echo $OUTPUT->heading("Active logins", 4);
+/*echo $OUTPUT->heading("Active logins", 4);
 
 echo $OUTPUT->box_start();
 echo" count of users (including teachers, and students) active in the LMS for the last 120 days 
 ;";
-echo '<br/>';
+echo '<br/>'; */
 $enrolled = $DB->get_records_sql("
 
 SELECT id FROM  {user}   
@@ -42,18 +42,18 @@ WHERE DATEDIFF( NOW(),FROM_UNIXTIME(   lastlogin   ) ) < 120");
 
 $count = count($enrolled);
 
-echo "$count";
+/*echo "$count";
 
 echo $OUTPUT->box_end();
 
 
-echo '<br/>';
+echo '<br/>'; */
 
-echo $OUTPUT->heading("Course Completions", 4);
+/*echo $OUTPUT->heading("Course Completions", 4);
 
 echo $OUTPUT->box_start();
 echo" Counts the number of learner completions from all the available courses in the system.";
-echo '<br/>';
+echo '<br/>'; */
 $timecompleted = $DB->get_records_sql("
 
 SELECT p.timecompleted 
@@ -62,20 +62,71 @@ FROM {course_completions} p
 ");
 
 $content = count($timecompleted);
-echo "$content";
+/*echo "$content";
 
-echo $OUTPUT->box_end();
-
-
+echo $OUTPUT->box_end();*/
 
 
 
-    echo '<br/>';
+/*$users = $DB->get_records('user');
+foreach ($users as $user) {
+ //$content = $user->firstname. '   ' . userdate($user->timecreated, get_string('strftimerecentfull'))."<br>";
+ echo '<b>' . $user->firstname . '   ' . userdate($user->timecreated, get_string('strftimerecentfull')).'</b><br />'; 
+}
+$displaytable=new html_table();
+$displaytable->head= array('firstname','Time created');
+$displaytable->width='100%';
+$col1='';
+$col2='';
+$col1width='40%';
+$col2width='60%';
+$allcolattributes= array();
+$rowvalue=array();
+$allrows=array();
+//$users = $DB->get_records('user', ['id' => '4']);
+$users = $DB->get_records('user');
+foreach ($users as $user) {
+ //$content = $user->firstname. '   ' . userdate($user->timecreated, get_string('strftimerecentfull'))."<br>";
+ //echo $content;
+   $col1.=firstname;
+$col2.= userdate($user->timecreated, get_string('strftimerecentfull'));
+array_push($allcolattributes, $col1width, $col2width);
+array_push($rowvalue, $col1);
+array_push($rowvalue, $col2);
+array_push($allrows, $rowsvalue);
+$col1='';
+$col2='';
+$rowvalue=array();
+
+
+
+      $displaytable->data = $allrows; 
+      $displaytable->size = $allcolattributes; 
+      echo html_writer::table($displaytable); 
+    
+}
+      
+     // echo $content;
+      echo $OUTPUT->box_end();
+
+/*$courses = $DB-> get_records('course_categories');
+       $table=new html_table();
+       $table->head=array('Categories', 'Number of course count', );
+    foreach ($courses as $course) {
+       //$content = $course->fullname.'  ' .userdate($course->timecreated, get_string('strftimerecentfull')). '<br>';
+       $name=$course->name;
+         //$startdate=userdate($course->startdate, get_string('strftimerecentfull'));
+         $coursecount=$course->coursecount;
+         $table->data[]=array($name, $coursecount);
+    }
+    $content .=html_writer::table($table); */
+
+   /* echo '<br/>';
 echo $OUTPUT->heading("Enrolled users", 4);
 echo $OUTPUT->box_start();
 echo '<br/>';
 
-echo"This report counts the enrolled users in course 3;";
+echo"This report counts the enrolled users in course 3;";*/
 
   $enrolled = $DB->get_records_sql("
 
@@ -91,73 +142,83 @@ where c.id = 3");
 
 $countenrolls = count($enrolled);
 
-echo "There are $countenrolls users in course 3";
+/*echo "There are $countenrolls users in course 3";
 
 echo $OUTPUT->box_end();
 
-echo '<br/>';
-
-echo $OUTPUT->heading("total users", 4);
-echo $OUTPUT->box_start();
-echo '<br/>';
-
-echo"This report counts total users;";
-
-  $enrolls = $DB->get_records_sql("
-
-SELECT ra.userid
-
-FROM {role_assignments} ra
-
-JOIN {context} ctx ON ra.contextid=ctx.id
-
-where ra.roleid = 5");
-
-$countusers = count($enrolls);
-
-echo "$countusers";
-
-echo $OUTPUT->box_end();
-
+   
       
 
 
-echo '<br/>';
+echo '<br/>';*/
 
-echo '<br/>';
 
-echo $OUTPUT->heading("total instructors", 4);
+/*echo $OUTPUT->heading("Total instructors", 4);
 echo $OUTPUT->box_start();
-echo '<br/>';
+echo '<br/>'; 
 
-echo"This report counts total instructors;";
+echo"This report counts total teachers;";*/
 
   $enrollteachers = $DB->get_records_sql("
 
 SELECT ra.userid
 
 FROM {role_assignments} ra
+JOIN {context} ctx ON ra.contextid = ctx.id
 
-JOIN {context} ctx ON ra.contextid=ctx.id
 
-where ra.roleid = 3 ");
+where ra.roleid = 3");
 
 $countteachers = count($enrollteachers);
 
-echo "$countteachers";
+/*echo "$countteachers ";
 
 echo $OUTPUT->box_end();
 
+   
       
 
 
+echo '<br/>';*/
+
+
+
+
+/*echo $OUTPUT->heading("Total instructors", 4);
+echo $OUTPUT->box_start();
 echo '<br/>';
-echo $OUTPUT->heading("Categories Report", 4);
+
+echo"This report counts total teachers;";*/
+
+  $enrollusers = $DB->get_records_sql("
+
+SELECT ra.userid
+
+FROM {role_assignments} ra
+JOIN {context} ctx ON ra.contextid = ctx.id
+
+
+where ra.roleid = 5");
+
+$countusers = count($enrollusers);
+
+/*echo "$countusers ";
+
+echo $OUTPUT->box_end();
+
+   
+      
+
+
+echo '<br/>';*/
+
+
+/*echo $OUTPUT->heading("Categories Report", 4);
 echo $OUTPUT->box_start();
 echo '<br/>';
 
 echo"This report lists the course categories and number of course count;";
-echo '<br/>';
+echo '<br/>';*/
 
 $courses = $DB-> get_records('course_categories');
        $table=new html_table();
@@ -170,8 +231,8 @@ $courses = $DB-> get_records('course_categories');
          $table->data[]=array($name, $coursecount);
     }
     $contents .=html_writer::table($table); 
-    echo $contents;
-    echo $OUTPUT->box_end();
+    /*echo $contents;
+    echo $OUTPUT->box_end();*/
     ?>
 
 
@@ -189,7 +250,7 @@ $courses = $DB-> get_records('course_categories');
 <div class ="row">
   
 <div class = "col-lg-4 col-md-3" >
-   <div class="card_activeusers" style="width: 16rem; height:25vh;">
+   <div class="card_activeusers" style="width: 16rem; height:20vh;">
    <div class="row m-0">
    <div class="col-lg-8 col-md-4 p-0">
       <div class="content">
@@ -208,15 +269,35 @@ $courses = $DB-> get_records('course_categories');
    </div>
 </div>
 </div>
-
 <div class = "col-lg-4 col-md-3" >
-   <div class="card_activelearners" style="width: 16rem; height:25vh;">
+   <div class="card_activeusers" style="width: 16rem; height:20vh;">
    <div class="row m-0">
    <div class="col-lg-8 col-md-4 p-0">
       <div class="content">
          
-          <p>Active learners</p>
-          <p>[php code to place]</p>
+          <p>Learners</p>
+          <p><?php echo $countusers ?></p>
+      </div>
+  </div>
+  <div class="col-lg-4 col-md-8 p-0">
+      <div class="image_script">
+      
+         <img src="learnscript/activelearners.jpg" alt="image" width="80px" height="80px">
+
+      </div>
+  </div>
+   </div>
+</div>
+</div>
+
+<div class = "col-lg-4 col-md-3" >
+   <div class="card_activelearners" style="width: 16rem; height:20vh;">
+   <div class="row m-0">
+   <div class="col-lg-8 col-md-4 p-0">
+      <div class="content">
+         
+          <p>Instructors</p>
+          <p><?php echo $countteachers ?></p>
       </div>
   </div>
   <div class="col-lg-4 col-md-8 p-0">
@@ -233,10 +314,61 @@ $courses = $DB-> get_records('course_categories');
    </div> 
    <p></p>
    <div class="container">
+<div class ="row">
+  
+<div class = "col-lg-4 col-md-3" >
+   <div class="card_activeusers" style="width: 16rem; height:20vh;">
+   <div class="row m-0">
+   <div class="col-lg-8 col-md-4 p-0">
+      <div class="content">
+         
+          <p>Course Completions</p>
+          <p><?php echo $content ?></p>
+      </div>
+  </div>
+  <div class="col-lg-4 col-md-8 p-0">
+      <div class="image_script">
+      
+         <img src="learnscript/activelearners.jpg" alt="image" width="80px" height="80px">
+
+      </div>
+  </div>
+   </div>
+</div>
+</div>
+<div class = "col-lg-4 col-md-3" >
+   <div class="card_activeusers" style="width: 16rem; height:20vh;">
+   <div class="row m-0">
+   <div class="col-lg-8 col-md-4 p-0">
+      <div class="content">
+         
+          <p>Enrollments</p>
+          <p><?php echo $countenrolls ?></p>
+      </div>
+  </div>
+  <div class="col-lg-4 col-md-8 p-0">
+      <div class="image_script">
+      
+         <img src="learnscript/activelearners.jpg" alt="image" width="80px" height="80px">
+
+      </div>
+  </div>
+   </div>
+</div>
+</div>
+
+
+
+</div>
+   </div> 
+   <p></p>
+
+   
+   <div class="container">
       <div class ="row">
         
       <div class = "col-lg-6 col-md-3" >
-         <div class="card_nologincourses" style="width: 24rem; height:35vh;">
+         <div class="card_nologincourses" style="width: 24rem; height:22vh;">
          <div class="row m-0">
          <div class="col-lg-8 col-md-4 p-0">
             <div class="content">
@@ -270,7 +402,7 @@ $courses = $DB-> get_records('course_categories');
       </div>
       
       <div class = "col-lg-6 col-md-3" >
-         <div class="card_courseparticipation" style="width: 24rem; height:35vh;">
+         <div class="card_courseparticipation" style="width: 24rem; height:20vh;">
          <div class="row m-0">
          <div class="col-lg-8 col-md-4 p-0">
             <div class="content">
@@ -286,8 +418,8 @@ $courses = $DB-> get_records('course_categories');
                   </thead>
                   <tbody>
                     <tr>
-                      <th scope="row">[php code to place]</th>
-                      <td>[php code to place]</td>
+                      <th scope="row"><?php echo $countenrolls ?></th>
+                      <td><?php echo $content ?></td>
                      
                     </tr>
                   </tbody>
@@ -381,50 +513,11 @@ $courses = $DB-> get_records('course_categories');
 <div class="container">
 <div class ="row">
   
-<div class = "col-lg-4 col-md-3" >
-   <div class="card_coursecompletions" style="width: 16rem; height:25vh;">
-   <div class="row m-0">
-   <div class="col-lg-8 col-md-4 p-0">
-      <div class="content">
-         
-          <p>Course Completions</p>
-          <p><?php echo $content ?></p>
-      </div>
-  </div>
-  <div class="col-lg-4 col-md-8 p-0">
-      <div class="image_script">
-      
-         <img src="learnscript/activelearners.jpg" alt="image" width="80px" height="80px">
 
-      </div>
-  </div>
-   </div>
-</div>
-</div>
-
-<div class = "col-lg-4 col-md-3" >
-   <div class="card_enrolledusers" style="width: 16rem; height:25vh;">
-   <div class="row m-0">
-   <div class="col-lg-8 col-md-4 p-0">
-      <div class="content">
-         
-          <p>Enrolled users</p>
-          <p><?php echo $countenrolls ?></p>
-      </div>
-  </div>
-  <div class="col-lg-4 col-md-8 p-0">
-      <div class="image_script">
-         <img src="learnscript/activelearners.jpg" alt="image" width="80px" height="80px">
-
-      </div>
-  </div>
-   </div>
-</div>
-</div>
-<div class = "col-lg-4 col-md-3" >
+<div class = "col-lg-12 col-md-12" >
    <div class="card_categoriesreport" style="width: 24rem; height:35vh;">
    <div class="row m-0">
-   <div class="col-lg-8 col-md-4 p-0">
+
       <div class="content">
          
           <p>Categories Report</p>
